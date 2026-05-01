110f3e9b1deee96653ab2ca54c021756e97a1e36
FrisbyTeam <frisbyteam77@gmail.com>
2026-05-01 13:31:24 +0300

    main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from stats_image import render_stats_image, render_empty_image
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+from aiohttp import TCPConnector
 from aiogram.client.session.aiohttp import AiohttpSession
-session = AiohttpSession(connector_kwargs={"family": 2})  # 2 = IPv4 only
+connector = TCPConnector(family=2)  # 2 = AF_INET = IPv4 only
+session = AiohttpSession(connector=connector)
 bot = Bot(token=BOT_TOKEN, session=session)
 dp = Dispatcher(storage=MemoryStorage())
 
